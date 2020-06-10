854d609152ec7c6778f3492d327fe151dbd43d0a
Yirbett Joseph <yirbett@gmail.com>
2020-06-10 07:22:05 -0400

    refactored functions

--- js/app.js
+++ js/app.js
@@ -1,36 +1,36 @@
 import {POETRY as POEM}  from "./texan.js" 
-const textForm = document.querySelector("#textForm");
+const textForm = document.getElementById("textForm");
+const txtBox = document.getElementById("work-area_text-box");
 let activeButton = document.getElementsByClassName("active");
 let activeButtonID = activeButton[0].getAttribute("id");
 
 
 
 
+
 //General Functions
-
-
 //Event Listeners
 //obtain text from textbox depending on the tipe of text to analize
 textForm.addEventListener("submit", (e)=> {
-    e.preventDefault();
-    switch (activeButtonID) {
-        case "poem-btn":
-            console.log(POEM.getVerses(textForm));
-            break;
-        case "tales-btn":
-            console.log(POEM.getVerses());
-            break;
-        case "novel-btn":
-            console.log(POEM.getVerses());
-            break;
-        case "esey-Btn":
-            console.log(POEM.getVerses());
-            break;
-        default:
-            console.log("nextthing");
-            break;
-    }
-});
+     e.preventDefault();
+     switch (activeButtonID) {
+         case "poem-btn":
+             console.log(POEM.getVerses(txtBox));
+             break;
+         case "tales-btn":
+             console.log(POEM.getVerses());
+             break;
+         case "novel-btn":
+             console.log(POEM.getVerses());
+             break;
+         case "esey-Btn":
+             console.log(POEM.getVerses());
+             break;
+         default:
+             console.log("nextthing");
+             break;
+     }
+   });
 
 
 

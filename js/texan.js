4b1959dd023520e43d3e7567ec851a94fff9f206
Yirbett Joseph <yirbett@gmail.com>
2020-06-21 20:24:52 -0400

    Reyex de silabas  refinado para detectar triptongo

--- js/texan.js
+++ js/texan.js
@@ -1,7 +1,7 @@
     
     //Text Annalisis Methods
 
-const syllableRegex = /([clpbmr]+[oea](?=[íóeoa])|\sy\s|[^aeiouáóáéíú\s]{0,2}([óáéíú]|[aeiou]+(y?(?![aeiouóáíéú]+)|[óáéíú]?))(?:[^aeiouyáóáéíú]$|[^aeiouytp\s]{0,1}(?=[^aeiouyhrlnáóáéíúü]))?)/gi; //para dividir en silabas. trabaja mal para palabras con "rl"  en medio como "merlusa" = me-rlu-sa debe ser corregido en el algoritmo
+const syllableRegex = /([htdgpl]+(iai|iái|iau|iéi|ieu|ioi|iou|uai|uau|uéi)[rns]?|[clpbmr]+[oea](?=[íóeoa])|\sy\s|[^aeiouáóáéíú\s]{0,2}([óáéíú]|[aeiou]+(y?(?![aeiouóáíéú]+)|[óáéíú]?))(?:[^aeiouyáóáéíú]$|[^aeiouytp\s]{0,1}(?=[^aeiouyührlnáóáéíú]))?)/gi; //para dividir en silabas. trabaja mal para palabras con "rl" y "rn" en medio como "merlusa" = me-rlu-sa  y pierna = pie-rna debe ser corregido en el algoritmo
 
 const metricRegex = /[^aeiouyáóáéíú\s]{0,2}([óáéíú]|[aeiouyh]+[óáéíú]?)(?:[^aeiouyáóáéíú]$|[^aeiouytpd\s]{0,1}(?=[^aeiouyhrlnáóáéíú]))?/gi; //para dividir en unidades métricas de poesía. no funciona bien con "lr" ni "rl" debe ser corregido en el algoritmo
 
@@ -28,13 +28,13 @@ class TEXAN //manages the text analisys
 
         //correcting exceptions
 
-        //making words like "merlusa"  equal to Mer-lu-sa instead of me-rlu-sa 
+        //making words like "merlusa"  equal to Mer-lu-sa instead of me-rlu-sa and pier-na instead of pie-rna
         text.map((line)=> 
         {
             if (line!=null)// evitar los saltos de linea nulos
             {
                 line.map(((silab, index, vers) => {
-                    if((silab[0]=="r") && (silab[1]=="l")){
+                    if((silab[0]=="r") && (silab[1]=="l"|silab[1]=="n")){
                         let newSilab= vers[index].slice(1);
                         vers[index]= newSilab;
                         vers[index-1]+="r";
